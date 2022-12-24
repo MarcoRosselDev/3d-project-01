4c496e90582927253d5c75e3bf00e29bfdfa8357
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-24 18:55:15 -0300

    three

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,8 +1,32 @@
-import React from "react";
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
 
 const Scene = () => {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const currentMount = mountRef.current;
+    // Scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      25,
+      currentMount.clientWidth / currentMount.clientHeight,
+      0.1,
+      1000
+    );
+    scene.add(camera);
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
+  }, []);
+
   return (
-    <div className="container3d" style={{ width: "100%", height: "100vh" }}>
+    <div
+      className="container3d"
+      ref={mountRef}
+      style={{ width: "100%", height: "100vh" }}
+    >
       <h1>Hola mundo</h1>
     </div>
   );
