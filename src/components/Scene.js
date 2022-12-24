3a1ddc7941f2205bb37b563edbbe93f5cd0368ee
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-24 18:35:48 -0300

    setup

---
+++ src/components/Scene.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Scene = () => {
+  return (
+    <div className="container3d" style={{ width: "100%", height: "100vh" }}>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+};
+
+export default Scene;
