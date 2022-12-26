7313b1ffa6d3e16ccd24f6a14738ce5c374c72a2
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-26 20:22:37 -0300

    separando para más claridad 2

--- src/components/Cubo.js
+++ src/components/Cubo.js
@@ -4,10 +4,60 @@ import cubo from "./cubo.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const Cubo = () => {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const currentMount = mountRef.current;
+    // Scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      55,
+      currentMount.clientWidth / currentMount.clientHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 4;
+    scene.add(camera);
+    // Renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
+
+    currentMount.appendChild(renderer.domElement);
+
+    // Controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    // Cubo
+    const cubo = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({
+        color: 0x3f7b9d,
+        transparent: true,
+        opacity: 0.3,
+        wireframe: true,
+      })
+    );
+    scene.add(cubo);
+
+    // Render the scene
+
+    const animate = () => {
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    };
+    animate();
+
+    // clean up scene
+    return () => {
+      currentMount.removeChild(renderer.domElement);
+    };
+  }, []);
   return (
-    <div>
-      <h1>cubo canvas</h1>
-    </div>
+    <div
+      className="Contenedor3D"
+      ref={mountRef}
+      style={{ width: "100%", height: "100vh" }}
+    ></div>
   );
 };
 
