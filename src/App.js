971a7ea4e8db6609a68883642c7d1424b4901ee7
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-26 20:30:37 -0300

    separando para más claridad 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Scene from "./components/Scene";
 import Cubo from "./components/Cubo";
+import styles from "./stylec.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Scene />
-
       <Cubo />
-    </>
+    </div>
   );
 }
 
